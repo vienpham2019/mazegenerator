aa6bbb26ba59279aa70bac792d2d6cb0ae84e843
vienpham2019 <vienpham2019@gmail.com>
2020-06-24 18:49:01 -0500

    save

--- server.js
+++ server.js
@@ -3,13 +3,7 @@ const path = require('path')
 
 const app = express()
 
-if(process.env.NODE_ENV === 'production'){
-	app.use(express.static('client/build'))
-
-    app.get('*' , (req,res) => {
-        res.sendFile(path.resolve(__dirname, 'client' , 'build', 'index.html'))
-	})
-}
+app.use(express.static(path.join(__dirname, 'client' , 'public')))
 
 const PORT = process.env.PORT || 5000
 
